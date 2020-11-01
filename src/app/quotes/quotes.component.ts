34a91e31171f43fc93c4495eb2a4013c20b3fbc5
Demarillac Izere <izered3@gmail.com>
2020-11-02 00:09:24 +0200

    changed the layout of the page

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,11 +7,11 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quotes: Quote[] =[
-    new Quote(1,"Demarillac","Some people feel the rain. Others just get wet.","Bob Dylan",new Date(2019,1,6),2,4),
-    new Quote(2, "Louise Izere", "You can do anything but not everything", "Anonymous",new Date(2019,9,30),0,0),
-    new Quote(3,"Moringa Student","Nobody really cares if you’re miserable, so you might as well be happy.","Chuck Palahniuk",new Date(2020,1,1),3,0),
+    new Quote(1,"Demarillac","When you reach the end of your rope, tie a knot in it and hang on","Franklin Roosevelt",new Date(2019,1,6),2,4),
+    new Quote(2, "Louise Izere", "If you look at what you have in life, you will always have more. If you lokk at wjat you don't have in life, you will never have enough.", "Oprah Winfrey",new Date(2019,9,30),0,0),
+    new Quote(3,"Moringa Student","Nobody really cares if you’re miserable, so you might as well be happy.","Anonymous",new Date(2020,1,1),3,0),
     new Quote(4,"Unknown","Nobody can make you feel inferior without your permission.","Eleanor Roosevelt",new Date(2020,9,30),5,0),
-    new Quote(5,"I.L.D","To live a creative life, we must lose our fear of being wrong","Demarillac Izere",new Date(),3,1),
+    new Quote(5,"I.L.D","Life is either a daring adventure or nothing","Demarillac Izere",new Date(),3,1),
   ];
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
@@ -22,13 +22,14 @@ export class QuotesComponent implements OnInit {
   toogleDetails(index){
     this.quotes[index].showDescription =!this.quotes[index].showDescription;
 }
-preNum:number
-  lastNum:number
-  counter:number
+preNum:number;
+  lastNum:number;
+  counter:number;
+
 HighestUpvote(){
+
   this.preNum = 0
   this.lastNum = 0
-
   for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
     this.lastNum = this.quotes[this.counter].upvote;
     if(this.lastNum > this.preNum){this.preNum = this.lastNum}
